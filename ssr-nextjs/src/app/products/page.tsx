72d1ce1b27fb4b64b3e57676bf70244afac50432
LukaSparrow <wrobel.lukasz02@gmail.com>
2025-10-11 16:20:59 +0200

    Delete mock products dependency

--- ssr-nextjs/src/app/products/page.tsx
+++ ssr-nextjs/src/app/products/page.tsx
@@ -1,7 +1,6 @@
 import { ProductsPage } from "@/components/products-page";
-import { mockProducts } from "@/data/mock-products";
 
 // Server Component - renderowane na serwerze
 export default function ProductsPageRoute() {
-  return <ProductsPage initialProducts={mockProducts} />;
+  return <ProductsPage />;
 }
